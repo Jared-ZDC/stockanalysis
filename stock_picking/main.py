7bf7cfd5c22cde239a020280abf7b8c916f6fd04
Jared Zhu <zhu156208@126.com>
2023-07-03 07:04:06 +0000

    fix some bug， add class

--- stock_picking/main.py
+++ stock_picking/main.py
@@ -4,8 +4,6 @@
 # coding=utf-8
 # 导入tushare
 import tushare as ts
-import pandas as pd
-import time
 
 debug = True
 
@@ -22,7 +20,7 @@ class cmp_opt:
     #静态市盈率
     pe : float = 0.0
     #市净率
-    pb : float = 0.0 
+    pb : float = 0.0
     #换手率
     turnover_rate : float = 0.0
     #量比
@@ -30,7 +28,7 @@ class cmp_opt:
     #动态股息率
     dv_ttm : float = 0.0
     #静态股息率
-    dv_ratio : float = 0.0  
+    dv_ratio : float = 0.0
     #date
     date : str = ""
     #评分
@@ -40,7 +38,6 @@ class cmp_opt:
         return f"ts_code = {self.ts_code}, total_mv={self.total_mv}, pe_ttm={self.pe_ttm}, \
                 pe={self.pe}, pb={self.pb}, turnover_rate={self.turnover_rate}, volume_ratio={self.volume_ratio}, \
                 dv_ttm={self.dv_ttm}, date={self.date}, score={self.score}"
-    
 
 def debug_print(msg : str) :
     """
@@ -50,6 +47,18 @@ def debug_print(msg : str) :
         print(msg)
     else:
         pass
+
+def none2zero(var : object) -> float:
+    """
+    None对象转换成0
+    """
+    try:
+        if var == None:
+            return 0.0
+        else:
+            return float(var)
+    except:
+        return 0.0
 
 def init_context() -> None:
     ts.set_token('e9a352db1e3bc57734dd5232c058b9e36e4b655f0d0661ea3ecb1b8d')
@@ -80,7 +89,7 @@ def get_hz300_company(start_date_ : str = "",end_date_ : str = "",market_cap_min
         if len(df_daily_basic['total_mv']) == 0:
             print(f"{con_code[index]} has no total_mv, total_mv = zero")
         else:
-            total_mv = float(df_daily_basic['total_mv'])
+            total_mv = float(df_daily_basic['total_mv'].iloc[0])
         #判断当前估值是否在规定范围以内    
         if total_mv >= market_cap_min and total_mv <= market_cap_max:
             df_daily_basic_dict[con_code[index]] = df_daily_basic
@@ -94,32 +103,30 @@ if __name__ == "__main__":
     #初始化token信息
     init_context()
 
-    ts_target : dict = None    
+    #cmp_opt对象
+    ts_target : dict = {}
 
     # 获取沪深300 市值过2000亿的公司
     hz300 = get_hz300_company(start_date_= "20230601" ,end_date_="20230631")
     #查询一下市值1000亿的公司有哪些
-    if debug :
-        print(f"more than 2000yi : {len(hz300)}")
-        for ts_code,ts_value in hz300.items():
-            print(f"{ts_value}")
-            #解析数据到对象中，对象存放在dict中，以ts_code作为索引
-            ts = cmp_opt()
-            ts.ts_code = ts_value['ts_code']
-            fc = lambda x : float(x) if x else 0
-            ts.total_mv = fc(ts_value['total_mv'])
+    #if debug :
+    print(f"more than 2000yi : {len(hz300)}")
 
-
-
-
-
-
-
-
-
-
-
-
+    for ts_code,ts_value in hz300.items():
+        print(f"{ts_value}")
+        #解析数据到对象中，对象存放在dict中，以ts_code作为索引
+        ts = cmp_opt()
+        ts.ts_code = ts_value['ts_code'].iloc[0]
+        ts.total_mv = none2zero(ts_value['total_mv'].iloc[0]) / 10000.0
+        ts.dv_ratio = none2zero(ts_value['dv_ratio'].iloc[0])
+        ts.dv_ttm = none2zero(ts_value['dv_ttm'].iloc[0])
+        ts.pb = none2zero(ts_value['pb'].iloc[0])
+        ts.pe = none2zero(ts_value['pe'].iloc[0])
+        ts.pe_ttm = none2zero(ts_value['pe_ttm'].iloc[0])
+        ts.turnover_rate = none2zero(ts_value['turnover_rate'].iloc[0])
+        ts.volume_ratio = none2zero(ts_value['volume_ratio'].iloc[0])
+        ts_target[ts.ts_code] = ts
+        print(ts)
 
 
 
