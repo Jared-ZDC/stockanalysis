ecd5730197bf3d9ffa80b3a4b4b787f329a4134d
Your Name <you@example.com>
2023-07-03 11:42:09 +0800

    add class for cmp_opt

--- stock_picking/main.py
+++ stock_picking/main.py
@@ -9,6 +9,39 @@ import time
 
 debug = True
 
+class cmp_opt:
+    """
+    定义标的的一些常用属性，比如pe_ttm/total_mv/pb/dv_ratio/turnover_rate/ema5/ema10/ema30/ema300 主要是每日指标
+    """
+    #标的代码
+    ts_code : str = ""
+    #总市值(单位亿)
+    total_mv : float = 0.0
+    #动态市盈率
+    pe_ttm : float = 0.0
+    #静态市盈率
+    pe : float = 0.0
+    #市净率
+    pb : float = 0.0 
+    #换手率
+    turnover_rate : float = 0.0
+    #量比
+    volume_ratio : float = 0.0
+    #动态股息率
+    dv_ttm : float = 0.0
+    #静态股息率
+    dv_ratio : float = 0.0  
+    #date
+    date : str = ""
+    #评分
+    score : float = 0.0
+
+    def __str__( self ):
+        return f"ts_code = {self.ts_code}, total_mv={self.total_mv}, pe_ttm={self.pe_ttm}, \
+                pe={self.pe}, pb={self.pb}, turnover_rate={self.turnover_rate}, volume_ratio={self.volume_ratio}, \
+                dv_ttm={self.dv_ttm}, date={self.date}, score={self.score}"
+    
+
 def debug_print(msg : str) :
     """
     仅作为debug打印使用
@@ -23,7 +56,7 @@ def init_context() -> None:
 
 def get_hz300_company(start_date_ : str = "",end_date_ : str = "",market_cap_min : float = 20000000.0, market_cap_max : float = 1000000000.0) -> dict :
     """
-    @function: 获取沪深300公司符合market_cap市值的公司,传入单位：亿；
+    @function: 月度接口,获取沪深300公司符合market_cap市值的公司,传入单位：亿；
     @market_cap_min : 最小市值
     @market_cap_max : 最大市值
     @date_ : 对应日期的沪深300成分股
@@ -32,32 +65,26 @@ def get_hz300_company(start_date_ : str = "",end_date_ : str = "",market_cap_min
     @link: https://tushare.pro/document/2?doc_id=32
     """
     pro = ts.pro_api()
+    #月度接口, 获取日期内的成分股
     df = pro.index_weight(index_code='399300.SZ', start_date = start_date_, end_date = end_date_)
     index_code = df['index_code']
     con_code = df['con_code']
     
-    #debug_print(f"df len = {len(df)}")
-    
     index = 0
     index_len = len(con_code)
     df_daily_basic_dict : dict = {}
-    #debug_print(f"index = {index}, index_len = {index_len}")
     while index < index_len:
-        #debug_print(f"{index_code[index],con_code[index]}")
-        
         #获取当前code公司估值
         df_daily_basic = pro.daily_basic(ts_code=con_code[index], trade_date=start_date_, fields=
                                          'ts_code,total_mv,pe,pe_ttm,pb,dv_ratio,dv_ttm,turnover_rate,volume_ratio')
-        #print(f"df_daily_basic['total_mv'] = {df_daily_basic['total_mv']},type = {type(df_daily_basic['total_mv'])},len = {len(df_daily_basic['total_mv'])}")
         total_mv = 0.0
         if len(df_daily_basic['total_mv']) == 0:
             print(f"{con_code[index]} has no total_mv, total_mv = zero")
         else:
             total_mv = float(df_daily_basic['total_mv'])
-            
+        #判断当前估值是否在规定范围以内    
         if total_mv >= market_cap_min and total_mv <= market_cap_max:
             df_daily_basic_dict[con_code[index]] = df_daily_basic
-            #debug_print(f"{con_code[index],df_daily_basic['total_mv']}")
         index += 1
     
     return df_daily_basic_dict
@@ -67,11 +94,32 @@ def get_hz300_company(start_date_ : str = "",end_date_ : str = "",market_cap_min
 if __name__ == "__main__":
     #初始化token信息
     init_context()
-    
-    # 获取沪深300 市值过1000亿的公司
+
+    ts_target : dict = None    
+
+    # 获取沪深300 市值过2000亿的公司
     hz300 = get_hz300_company(start_date_= "20230601" ,end_date_="20230631")
     #查询一下市值1000亿的公司有哪些
     if debug :
         print(f"more than 2000yi : {len(hz300)}")
         for ts_code,ts_value in hz300.items():
             print(f"{ts_value}")
+            #解析数据到对象中，对象存放在dict中，以ts_code作为索引
+            ts = cmp_opt()
+            ts.ts_code = ts_value['ts_code']
+            ts.total_mv = (lamada x : float(x) if(x != None) else 0)(ts_value['total_mv'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
