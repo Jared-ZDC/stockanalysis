2c7a0c8671d8b0765f3b511fdc8762a99531cf47
Jared Zhu <zhu156208@126.com>
2023-07-27 14:40:09 +0000

    add initDataBase

---
+++ database/initDataBase.py
@@ -0,0 +1,26 @@
+'''
+创建数据库
+'''
+
+
+
+def createDatabase(baseName : str) -> bool:
+    '''
+    @function: 创建数据库
+    @baseName : 数据库名字
+    '''
+    return True
+
+def createTable(baseName : str, tableName : str,  table_consistant : dict) -> bool :
+    '''
+    @func: 创建表单
+    @baseName : 数据库
+    @tableName : 表名
+    @table_consistant : 表的结构 {"ts_code",str}
+    '''
+    return True
+
+
+if __name__ == '__main__' :
+s
+    print(f"create database")
