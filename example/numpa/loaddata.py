33d2781e3912ced0bfbe84b3ca29742bafb8069c
Jared Zhu <zhu156208@126.com>
2023-08-21 11:50:07 +0000

    add data

--- example/numpa/loaddata.py
+++ example/numpa/loaddata.py
@@ -14,8 +14,8 @@ pro = ts.pro_api()
 
 #获取当前的股票信息
 data : pd.DataFrame = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
-data.to_csv("/mnt/e/tushare/stock_basic.csv",index=False,encoding='utf_8_sig')
-
+#data.to_csv("/mnt/e/tushare/stock_basic.csv",index=False,encoding='utf_8_sig')
+data.to_csv("/workspaces/stockanalysis/stock_basic.csv",index=False,encoding='utf_8_sig')
 def download(ts_code) :
     #print(f"{type(ts_code)}")
     index : int = 0
@@ -26,39 +26,41 @@ def download(ts_code) :
         if diary.empty == True :
             continue
         diary = diary.set_index(['trade_date'])
-        diary.index = pd.to_datetime(diary.index,format="%Y-%m-%d",utc=False)
+        diary.index = pd.to_datetime(diary.index,format="%Y%m%d",utc=False)
         diary.loc[:, 'openinterest'] = 0
         diary = diary.sort_index()
-        diary.to_csv(f"/mnt/e/tushare/{code}diaryNone.csv",index=True,encoding='utf_8_sig')
+        diary.to_csv(f"/workspaces/stockanalysis/{code}diaryNone.csv",index=True,encoding='utf_8_sig')
         print(f"{index} save {code} to /mnt/e/tushare/{code}diaryNone.csv")   
         
         #前复权
         diary: pd.DataFrame = ts.pro_bar(ts_code=code, adj="qfq", start_date='20150101', end_date='20230820')
         diary = diary.set_index(['trade_date'])
-        diary.index = pd.to_datetime(diary.index,format="%Y-%m-%d",utc=False)
+        diary.index = pd.to_datetime(diary.index,format="%Y%m%d",utc=False)
         diary.loc[:, 'openinterest'] = 0
         diary = diary.sort_index()
-        diary.to_csv(f"/mnt/e/tushare/{code}diaryqfq.csv",index=True,encoding='utf_8_sig')
+        diary.to_csv(f"/workspaces/stockanalysis/{code}diaryqfq.csv",index=True,encoding='utf_8_sig')
         print(f"{index} save {code} to /mnt/e/tushare/{code}diaryqfq.csv")   
 
         #后复权
         diary: pd.DataFrame = ts.pro_bar(ts_code=code, adj="hfq", start_date='20150101', end_date='20230820')
         diary = diary.set_index(['trade_date'])
-        diary.index = pd.to_datetime(diary.index,format="%Y-%m-%d",utc=False)
+        diary.index = pd.to_datetime(diary.index,format="%Y%m%d",utc=False)
         diary.loc[:, 'openinterest'] = 0
         diary = diary.sort_index()
-        diary.to_csv(f"/mnt/e/tushare/{code}diaryhfq.csv",index=True,encoding='utf_8_sig')
+        diary.to_csv(f"/workspaces/stockanalysis/{code}diaryhfq.csv",index=True,encoding='utf_8_sig')
         print(f"{index} save {code} to /mnt/e/tushare/{code}diaryhfq.csv")   
 
 #循环当前的股票信息
 ts_code = data['ts_code']
 #try:
-_thread.start_new_thread( download, (ts_code[:1000],))
-_thread.start_new_thread( download, (ts_code[1000:2000],))
-_thread.start_new_thread( download, (ts_code[2000:3000],))
-_thread.start_new_thread( download, (ts_code[3000:4000],))
-_thread.start_new_thread( download, (ts_code[4000:5000],))
-_thread.start_new_thread( download, (ts_code[5000:5262],))
+#_thread.start_new_thread( download, (ts_code[:1000],))
+#_thread.start_new_thread( download, (ts_code[1000:2000],))
+#_thread.start_new_thread( download, (ts_code[2000:3000],))
+#_thread.start_new_thread( download, (ts_code[3000:4000],))
+#_thread.start_new_thread( download, (ts_code[4000:5000],))
+#_thread.start_new_thread( download, (ts_code[5000:5262],))
+target_code = ("600036.SH","000858.SZ","600027.SH")
+download(target_code)
 #except Exception:
 #   print ("Error: 无法启动线程")
 
--- example/numpa/priceReboundStratege.py
+++ example/numpa/priceReboundStratege.py
@@ -15,10 +15,18 @@ from copy import deepcopy
 import time
 
 #目标： 招商银行，五粮液，华电国际
-#target_code = ("600036.SH","000858.SZ","600027.SH")
-target_code = ("600225.SH",)
-daily_price_hfq : list[pd.DataFrame] = []
-daily_price_none : list[pd.DataFrame] = []
+target_code = ("600036.SH","000858.SZ","600027.SH")
+#target_code = ("600225.SH",)
+#考虑的天数, 如果前precont_days的时间都是下降的， 第五天是反弹
+precont_days = 5
+
+#考虑precont_days中，允许多少次反弹
+rebound_times = 1
+
+#最高价回弹多少，清仓， 默认5%
+biggest_rebound = 0.05
+
+
 
 #构建交易策略 价格反转策略
 class priceRebound(bt.Strategy):
@@ -27,13 +35,92 @@ class priceRebound(bt.Strategy):
         self.buy_stock = target_code # 保留调仓列表
         self.order_list = [] # 记录以往订单，方便调仓日对未完成订单做处理
         self.buy_stocks_pre = [] # 记录上一期持仓
+        self.count = 0 # 记录next循环次数
+        self.precont_days : list[int] = []
+        self.rebound_times : list[int] = []
+        self.biggest_value : list[float] = []  #当前趋势中的最高价
+        self.lowest_value : list[float] = []  #当前趋势中的最低价
+        self.buy_point : list[float] = []  #买点
+        
+        print("-------------self.datas-------------")
+        print(f"len = {len(self.datas)}")
+        print("-------------self.datas[0]-------------")
+        print(self.datas[0]._name, self.datas[0]) # 返回第一个导入的数据表格，常规形式
+        print("-------------self.datas[1]-------------")
+        print(self.datas[1]._name, self.datas[1]) # 返回第二个导入的数据表格，常规形式
+        print("-------------self.datas[2]-------------")
+        print(self.datas[2]._name, self.datas[2]) # 返回第三个导入的数据表格，常规形式
+
+        print("--------- 打印 self 策略本身的 lines ----------")
+        print(self.lines.getlinealiases())
+        print("--------- 打印 self.datas 第一个数据表格的 lines ----------")
+        print(self.datas[0].lines.getlinealiases())
         pass
     def next(self):
         dt = self.datas[0].datetime.date(0) # 获取当前的回测时间点
         print(f"date = {dt}")
+
+        print(f"------------- next 的第{self.count+1}次循环 --------------")
+        print("当前时点（今日）：",'datetime',self.data1.lines.datetime.date(0),'close', self.data1.lines.close[0])
+        print("往前推1天（昨日）：",'datetime',self.data1.lines.datetime.date(-1),'close', self.data1.lines.close[-1])
+        print("往前推2天（前日）", 'datetime',self.data1.lines.datetime.date(-2),'close', self.data1.lines.close[-2])
+        print("前日、昨日、今日的收盘价：", self.data1.lines.close.get(ago=0, size=3))
+        print("往后推1天（明日）：",'datetime',self.data1.lines.datetime.date(1),'close', self.data1.lines.close[1])
+        print("往后推2天（明后日）", 'datetime',self.data1.lines.datetime.date(2),'close', self.data1.lines.close[2])
+        # 在 next() 中调用 len(self.data0)，返回的是当前已处理（已回测）的数据长度，会随着回测的推进动态增长
+        print("已处理的数据点：", len(self.data1))
+        # buflen() 返回整条线的总长度，固定不变；
+        print("line的总长度：", self.data0.buflen())
         
+
         #判断当前的股价是否处于反弹状态
+        #循环取出目标股票，判断目标股票的价格,间隔2是由于存放数据的方式是交替进行的， index=0 未复权数据， index=1 后复权数据
+        for index in range(0,len(self.datas),2):
+            none_index = index
+            hfq_index = index + 1
+            today_close:float = self.datas[hfq_index].lines.close[0]
+            if self.count == 0 :
+                #第一次进入循环， 只要记录lowest_value，biggest_value
+                self.lowest_value[hfq_index] = today_close
+                self.biggest_value[hfq_index] = today_close
+                self.precont_days[hfq_index] = 0
+                self.rebound_times[hfq_index] = 0
+                self.buy_point[hfq_index] = 0
+                continue
+            
+            #第二次进入循环， 开始进行算法迭代, 需要取后复权数据，所以要index + 1
+            yesterday_close:float = self.datas[hfq_index].lines.close[-1]
+            
+            #最低点、最高点用来做卖出条件
+            if today_close < self.lowest_value[hfq_index] :
+                #如果今天收盘价比当前最低点要低，则更新最低点
+                self.lowest_value[hfq_index] = today_close
+                
+            if today_close > self.biggest_value[hfq_index] :
+                #如果今天收盘价比当前最高点要高，则更新最高点
+                self.biggest_value[hfq_index] = today_close
+            
+            if today_close <= yesterday_close :
+                #如果今天收盘价比昨天收盘价要低，则precont_days计数+1
+                self.precont_days[hfq_index] += 1
+            else:
+                if self.precont_days[hfq_index] >= precont_days:
+                    #如果之前precont_days天都是下降得，今天开始反弹，则买入
+                    
+                    continue
+                if self.rebound_times[hfq_index] <= rebound_times:
+                    #如果今天收盘价比昨天收盘价要高，且次数小于等于rebound_times
+                    self.rebound_times[hfq_index] += 1
+                    self.precont_days[hfq_index] += 1
+                else:
+                    #如果次数大于了rebound_times
+                    self.precont_days[hfq_index] = 0
+                    self.rebound_times[hfq_index] = 0
+            
+            
+            
         time.sleep(1)
+        self.count += 1
         pass
     def notify_order(self, order):
         # 未被处理的订单
@@ -68,29 +155,26 @@ if __name__ == "__main__":
     
     #准备数据
     for ts_code in target_code:
-        #后复权数据技术指标
-        pd_temp : pd.DataFrame = pd.read_csv(f"/mnt/e/tushare/{ts_code}diaryhfq.csv", parse_dates=['trade_date'],
-                                           encoding='utf_8_sig', infer_datetime_format=True)
-        #重命名: 表内字段就是 Backtrader 默认情况下要求输入的 7 个字段： 
-        # 'datetime' 、'open'、'high'、'low'、'close'、'volume'、'openinterest'，外加一个 'sec_code' 股票代码字段。
-        pd_temp = pd_temp.rename(columns={'trade_date':'datetime','ts_code':'sec_code','vol':'volume'})
-        
-        pd_temp.loc[:, 'openinterest'] = 0
-        pd_temp["datetime"] = pd.to_datetime(pd_temp["datetime"])
-        pd_temp = pd_temp.set_index(['datetime'])
-        
-        pd_temp.index = pd.to_datetime(pd_temp.index,format="%Y-%m-%d",utc=False)
-        #datafeed = bt.feeds.PandasData(dataname=pd_temp)
-        
-        # 缺失值处理：有些交易日的数据为空，所以需要对缺失数据进行填充
-        pd_temp.loc[:,['volume','openinterest']] = pd_temp.loc[:,['volume','openinterest']].fillna(0)
-        pd_temp.loc[:,['open','high','low','close']] = pd_temp.loc[:,['open','high','low','close']].fillna(method='pad')
-        pd_temp.loc[:,['open','high','low','close']] = pd_temp.loc[:,['open','high','low','close']].fillna(0)
-        daily_price_hfq.append(pd_temp)
-        
-         # 导入未复权的数据
+        # 导入未复权的数据
         data = bt.feeds.GenericCSVData(
-            dataname=f"/mnt/e/tushare/{ts_code}diaryNone.csv",
+            dataname=f"/workspaces/stockanalysis/{ts_code}diaryNone.csv",
+            fromdate=datetime.datetime(2014, 1, 1),
+            todate=datetime.datetime(2023, 8, 20),
+            nullvalue=0.0,
+            dtformat=('%Y-%m-%d'),
+            datetime=0,
+            high=3,
+            low=4,
+            open=2,
+            close=5,
+            volume=9,
+            openinterest=-1
+        )
+        cerebro.adddata(data, name=f"{ts_code}_none") # 通过 name 实现数据集与股票的一一对应
+        
+        # 导入后复权的数据
+        data = bt.feeds.GenericCSVData(
+            dataname=f"/workspaces/stockanalysis/{ts_code}diaryhfq.csv",
             fromdate=datetime.datetime(2015, 1, 1),
             todate=datetime.datetime(2023, 8, 20),
             nullvalue=0.0,
@@ -103,25 +187,8 @@ if __name__ == "__main__":
             volume=9,
             openinterest=-1
         )
-        cerebro.adddata(data, name=ts_code) # 通过 name 实现数据集与股票的一一对应
-        """
-        #未复权的数据作为回测基线
-        pd_temp : pd.DataFrame  = pd.read_csv(f"/mnt/e/tushare/{ts_code}diaryNone.csv", parse_dates=['trade_date'],
-                                           encoding='utf_8_sig', infer_datetime_format=True)
-        pd_temp = pd_temp.rename(columns={'trade_date':'datetime','ts_code':'sec_code','vol':'volume'})
-        pd_temp.loc[:, 'openinterest'] = 0
-        pd_temp["datetime"] = pd.to_datetime(pd_temp["datetime"])
-        pd_temp = pd_temp.set_index(['datetime'])
+        cerebro.adddata(data, name=f"{ts_code}_hfq")
         
-        pd_temp.index = pd.to_datetime(pd_temp.index,format="%Y-%m-%d",utc=True)
-        #datafeed = bt.feeds.PandasData(dataname=pd_temp)
-        
-        # 缺失值处理：有些交易日的数据为空，所以需要对缺失数据进行填充
-        pd_temp.loc[:,['volume','openinterest']] = pd_temp.loc[:,['volume','openinterest']].fillna(0)
-        pd_temp.loc[:,['open','high','low','close']] = pd_temp.loc[:,['open','high','low','close']].fillna(method='pad')
-        pd_temp.loc[:,['open','high','low','close']] = pd_temp.loc[:,['open','high','low','close']].fillna(0)
-        daily_price_none.append(pd_temp)
-        """
        
     
     # 初始资金 100,000
