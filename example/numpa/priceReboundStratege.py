559307a8b2f86baba70ba400f8fdba8f59ba6966
Jared Zhu <zhu156208@126.com>
2023-09-03 07:16:02 +0000

    fix bug

--- example/numpa/priceReboundStratege.py
+++ example/numpa/priceReboundStratege.py
@@ -13,10 +13,11 @@ import numpy as np
 import datetime
 from copy import deepcopy
 import time
+import math
 
 #目标： 招商银行，五粮液，华电国际
-target_code = ("600036.SH","000858.SZ","600027.SH")
-#target_code = ("600225.SH",)
+#target_code = ("600036.SH","000858.SZ","600027.SH")
+target_code = ("600036.SH",)
 #考虑的天数, 如果前precont_days的时间都是下降的， 第五天是反弹
 precont_days = 5
 
@@ -30,18 +31,24 @@ biggest_rebound = 0.05
 
 #构建交易策略 价格反转策略
 class priceRebound(bt.Strategy):
+    
+    # 日志函数
+    def log(self, txt, dt=None):
+        # 以第一个数据data0，即指数作为时间基准
+        dt = dt or self.data0.datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+        
     '''选股策略'''
     def __init__(self):
-        self.buy_stock = target_code # 保留调仓列表
-        self.order_list = [] # 记录以往订单，方便调仓日对未完成订单做处理
-        self.buy_stocks_pre = [] # 记录上一期持仓
+
+        self.buy_stocks = [] # 记录持仓ts_code
         self.count = 0 # 记录next循环次数
-        self.precont_days : list[int] = []
-        self.rebound_times : list[int] = []
-        self.biggest_value : list[float] = []  #当前趋势中的最高价
-        self.lowest_value : list[float] = []  #当前趋势中的最低价
-        self.buy_point : list[float] = []  #买点
-        
+
+        self.precont_days : dict[int,int] = {}
+        self.rebound_times : dict[int,int] = {}
+        self.biggest_value : dict[int,float] = {}#当前趋势中的最高价
+        self.lowest_value : dict[int,float] = {} #当前趋势中的最低价
+        """
         print("-------------self.datas-------------")
         print(f"len = {len(self.datas)}")
         print("-------------self.datas[0]-------------")
@@ -55,71 +62,94 @@ class priceRebound(bt.Strategy):
         print(self.lines.getlinealiases())
         print("--------- 打印 self.datas 第一个数据表格的 lines ----------")
         print(self.datas[0].lines.getlinealiases())
+        """
         pass
     def next(self):
         dt = self.datas[0].datetime.date(0) # 获取当前的回测时间点
-        print(f"date = {dt}")
+        #print(f"date = {dt}")
 
-        print(f"------------- next 的第{self.count+1}次循环 --------------")
-        print("当前时点（今日）：",'datetime',self.data1.lines.datetime.date(0),'close', self.data1.lines.close[0])
-        print("往前推1天（昨日）：",'datetime',self.data1.lines.datetime.date(-1),'close', self.data1.lines.close[-1])
-        print("往前推2天（前日）", 'datetime',self.data1.lines.datetime.date(-2),'close', self.data1.lines.close[-2])
-        print("前日、昨日、今日的收盘价：", self.data1.lines.close.get(ago=0, size=3))
-        print("往后推1天（明日）：",'datetime',self.data1.lines.datetime.date(1),'close', self.data1.lines.close[1])
-        print("往后推2天（明后日）", 'datetime',self.data1.lines.datetime.date(2),'close', self.data1.lines.close[2])
+        #print(f"------------- next 的第{self.count+1}次循环 --------------")
+        #print("当前时点（今日）：",'datetime',self.data1.lines.datetime.date(0),'close', self.data1.lines.close[0])
+        #print("往前推1天（昨日）：",'datetime',self.data1.lines.datetime.date(-1),'close', self.data1.lines.close[-1])
+        #print("往前推2天（前日）", 'datetime',self.data1.lines.datetime.date(-2),'close', self.data1.lines.close[-2])
+        #print("前日、昨日、今日的收盘价：", self.data1.lines.close.get(ago=0, size=3))
+        #print("往后推1天（明日）：",'datetime',self.data1.lines.datetime.date(1),'close', self.data1.lines.close[1])
+        #print("往后推2天（明后日）", 'datetime',self.data1.lines.datetime.date(2),'close', self.data1.lines.close[2])
         # 在 next() 中调用 len(self.data0)，返回的是当前已处理（已回测）的数据长度，会随着回测的推进动态增长
-        print("已处理的数据点：", len(self.data1))
+        #print("已处理的数据点：", len(self.data1))
         # buflen() 返回整条线的总长度，固定不变；
-        print("line的总长度：", self.data0.buflen())
+        #print("line的总长度：", self.data0.buflen())
         
-
         #判断当前的股价是否处于反弹状态
-        #循环取出目标股票，判断目标股票的价格,间隔2是由于存放数据的方式是交替进行的， index=0 未复权数据， index=1 后复权数据
-        for index in range(0,len(self.datas),2):
-            none_index = index
-            hfq_index = index + 1
-            today_close:float = self.datas[hfq_index].lines.close[0]
+        #循环取出目标股票，判断目标股票的价格
+        for index in range(0,len(self.datas)):
+            today_open:float = self.datas[index].lines.open[0]
+            today_close:float = self.datas[index].lines.close[0]
             if self.count == 0 :
                 #第一次进入循环， 只要记录lowest_value，biggest_value
-                self.lowest_value[hfq_index] = today_close
-                self.biggest_value[hfq_index] = today_close
-                self.precont_days[hfq_index] = 0
-                self.rebound_times[hfq_index] = 0
-                self.buy_point[hfq_index] = 0
+                self.lowest_value[index] = today_close
+                self.biggest_value[index] = today_close
+                self.precont_days[index] = 0
+                self.rebound_times[index] = 0
                 continue
             
             #第二次进入循环， 开始进行算法迭代, 需要取后复权数据，所以要index + 1
-            yesterday_close:float = self.datas[hfq_index].lines.close[-1]
+            yesterday_close:float = self.datas[index].lines.close[-1]
             
             #最低点、最高点用来做卖出条件
-            if today_close < self.lowest_value[hfq_index] :
+            if today_close < self.lowest_value[index] :
                 #如果今天收盘价比当前最低点要低，则更新最低点
-                self.lowest_value[hfq_index] = today_close
+                self.lowest_value[index] = today_close
+                #self.log(f"UPDATE lowest_value : {today_close}")
                 
-            if today_close > self.biggest_value[hfq_index] :
+            if today_close > self.biggest_value[index] :
                 #如果今天收盘价比当前最高点要高，则更新最高点
-                self.biggest_value[hfq_index] = today_close
-            
-            if today_close <= yesterday_close :
-                #如果今天收盘价比昨天收盘价要低，则precont_days计数+1
-                self.precont_days[hfq_index] += 1
+                self.biggest_value[index] = today_close
+                #self.log(f"UPDATE biggest_value : {today_close}")
+
+            if today_open <= yesterday_close :
+                #self.log(f"today_close {today_close} <= yesterday_close {yesterday_close}")
+                #如果今天收盘价比昨天收盘价要低，这里要分是否持仓，如果持仓了，要分两种情况
+                # 1) 如果收盘价低于持仓价，清空持仓
+                # 2) 如果收盘价低于最高价 biggest_rebound，清空
+                if self.datas[index]._name in self.buy_stocks:
+                    if (today_open < self.broker.getposition(self.datas[index]).price) or (today_open < self.biggest_value[index] * (1-biggest_rebound)):
+                        #self.log(f"sell  {self.datas[index]._name} ")
+                        self.close(self.datas[index])
+                        self.buy_stocks.remove(self.datas[index]._name)
+                        self.rebound_times[index] = 0
+                        self.precont_days[index] = 0
+                        self.biggest_value[index] = 0
+                        self.lowest_value[index] = today_close
+                else:         
+                    self.precont_days[index] += 1
             else:
-                if self.precont_days[hfq_index] >= precont_days:
-                    #如果之前precont_days天都是下降得，今天开始反弹，则买入
-                    
+                #如果上涨过程中，判断是否持仓，如果已经持仓中，则不动
+                if self.datas[index]._name in self.buy_stocks:
                     continue
-                if self.rebound_times[hfq_index] <= rebound_times:
-                    #如果今天收盘价比昨天收盘价要高，且次数小于等于rebound_times
-                    self.rebound_times[hfq_index] += 1
-                    self.precont_days[hfq_index] += 1
+                
+                #反弹的话，就是判断是否到达预定的次数
+                if self.precont_days[index] >= precont_days:
+                    #如果之前precont_days天都是下降得，今天开始反弹，则买入
+                    #此时策略触发可以买入，但是需要看一下持仓
+                    cash : float = self.broker.getcash() #获取可用的资金
+                    value : float = self.broker.getvalue() #当前总资产
+                    
+                    #TODO : 暂时先不做动态再平衡（动态平衡，始终保持各1/3），直接判断当前总资产除以总数，如果剩余的钱超过1/n，则买1/n, 否则买剩余的所有份数
+                    if cash > (value / len(self.datas)) :
+                        self.buy(self.datas[index],size = value * 0.95 / len(self.datas) / today_close)
+                    else:
+                        self.buy(self.datas[index],size = cash * 0.95 / today_close)
+                        
+                    self.buy_stocks.append(self.datas[index]._name)
+                
+                if self.rebound_times[index] < rebound_times:             
+                    self.rebound_times[index] += 1
                 else:
-                    #如果次数大于了rebound_times
-                    self.precont_days[hfq_index] = 0
-                    self.rebound_times[hfq_index] = 0
-            
-            
-            
-        time.sleep(1)
+                    self.rebound_times[index] = 0
+                    self.precont_days[index] = 0
+                    
+        #time.sleep(1)
         self.count += 1
         pass
     def notify_order(self, order):
@@ -137,7 +167,15 @@ class priceRebound(bt.Strategy):
                         order.executed.comm, # 佣金
                         order.executed.size, # 成交量
                         order.data._name)) # 股票名称
+                self.log(f'当前可用资金 : {self.broker.getcash()}')
+                self.log(f'当前总资产 : {self.broker.getvalue()}')
+        
+                for _p in self.broker.positions:
+                    if self.broker.getposition(_p).size > 0:
+                        self.log(f"当前持仓 : {_p._name} 当前持仓量 : {self.broker.getposition(_p).size}, 当前持仓成本 : {self.broker.getposition(_p).price}")
+                #time.sleep(60)
             else: # Sell
+                
                 self.log('SELL EXECUTED, ref:%.0f, Price: %.2f, Cost: %.2f, Comm %.2f, Size: %.2f, Stock: %s' %
                             (order.ref,
                             order.executed.price,
@@ -145,17 +183,23 @@ class priceRebound(bt.Strategy):
                             order.executed.comm,
                             order.executed.size,
                             order.data._name))
+                self.log(f'当前可用资金 : {self.broker.getcash()}')
+                self.log(f'当前总资产 : {self.broker.getvalue()}')
+        
+                for _p in self.broker.positions:
+                    if self.broker.getposition(_p).size > 0:
+                        self.log(f"当前持仓 : {_p._name} 当前持仓量 : {self.broker.getposition(_p).size}, 当前持仓成本 : {self.broker.getposition(_p).price}")
+                #time.sleep(60)
     pass
 
 if __name__ == "__main__":
     # 实例化 cerebro
     cerebro = bt.Cerebro()
-    # 打印初始资金
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
     
     #准备数据
     for ts_code in target_code:
         # 导入未复权的数据
+        """ 暂时先全部采取复权数据处理
         data = bt.feeds.GenericCSVData(
             dataname=f"/workspaces/stockanalysis/{ts_code}diaryNone.csv",
             fromdate=datetime.datetime(2014, 1, 1),
@@ -171,7 +215,7 @@ if __name__ == "__main__":
             openinterest=-1
         )
         cerebro.adddata(data, name=f"{ts_code}_none") # 通过 name 实现数据集与股票的一一对应
-        
+        """
         # 导入后复权的数据
         data = bt.feeds.GenericCSVData(
             dataname=f"/workspaces/stockanalysis/{ts_code}diaryhfq.csv",
@@ -187,29 +231,54 @@ if __name__ == "__main__":
             volume=9,
             openinterest=-1
         )
-        cerebro.adddata(data, name=f"{ts_code}_hfq")
+        cerebro.adddata(data, name=f"{ts_code}")
         
        
     
-    # 初始资金 100,000
-    cerebro.broker.setcash(100000.0)
+    # 初始资金 1000,000
+    cerebro.broker.setcash(1000000.0)
     # 佣金，双边各 0.0002
     cerebro.broker.setcommission(commission=0.0002)
     # 滑点：双边各 0.0001
     cerebro.broker.set_slippage_perc(perc=0.0001)
     
-    cerebro.addanalyzer(bt.analyzers.TimeReturn, _name='pnl') # 返回收益率时序数据
-    cerebro.addanalyzer(bt.analyzers.AnnualReturn, _name='_AnnualReturn') # 年化收益率
-    cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='_SharpeRatio') # 夏普比率
-    cerebro.addanalyzer(bt.analyzers.DrawDown, _name='_DrawDown') # 回撤
+    # Add a FixedSize sizer according to the stake(国内1手是100股，最小的交易单位)
+    cerebro.addsizer(bt.sizers.FixedSize, stake=100)
+    
+    # 添加分析指标
+    # 返回年初至年末的年度收益率
+    cerebro.addanalyzer(bt.analyzers.AnnualReturn, _name='_AnnualReturn')
+    # 计算最大回撤相关指标
+    cerebro.addanalyzer(bt.analyzers.DrawDown, _name='_DrawDown')
+    # 计算年化收益：日度收益
+    cerebro.addanalyzer(bt.analyzers.Returns, _name='_Returns', tann=252)
+    # 计算年化夏普比率：日度收益
+    cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='_SharpeRatio', timeframe=bt.TimeFrame.Days, annualize=True, riskfreerate=0) # 计算夏普比率
+    cerebro.addanalyzer(bt.analyzers.SharpeRatio_A, _name='_SharpeRatio_A')
+    # 返回收益率时序
+    cerebro.addanalyzer(bt.analyzers.TimeReturn, _name='_TimeReturn')
     
     # 将编写的策略添加给大脑
     cerebro.addstrategy(priceRebound)
     
     # 启动回测
-    cerebro.run()
+    result = cerebro.run()
     # 打印回测完成后的资金
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    # 提取结果
+    print("--------------- AnnualReturn -----------------")
+    print(result[0].analyzers._AnnualReturn.get_analysis())
+    print("--------------- DrawDown -----------------")
+    print(result[0].analyzers._DrawDown.get_analysis())
+    print("--------------- Returns -----------------")
+    print(result[0].analyzers._Returns.get_analysis())
+    print("--------------- SharpeRatio -----------------")
+    print(result[0].analyzers._SharpeRatio.get_analysis())
+    print("--------------- SharpeRatio_A -----------------")
+    print(result[0].analyzers._SharpeRatio_A.get_analysis())
+    
+    #绘图
+    #cerebro.plot()
     
     #初始化数据
     pass
